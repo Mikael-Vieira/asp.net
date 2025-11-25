a1ffc706463fa4dd1a34bb05ca9ada58fbab8637
Mikael <mikaelvcasoti@gmail.com>
2025-11-25 15:59:41 -0300

    subindo tarefa

---
+++ Trabalho_ecommerce_teste/ClienteDAO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Trabalho_ecommerce_teste
+{
+    public class ClienteDAO
+    {
+        
+
+       
+
+        // 1️⃣ BUSCAR CLIENTE POR EMAIL
+
+        public static Clientes BuscarPorEmail(string email)
+        {
+            using (var ctx = new LaFlorencaEntities())
+            {
+                return ctx.Clientes
+                         .Where(c => c.Email == email)
+                         .FirstOrDefault();
+            }
+        }
+
+
+
+
+        // 3️⃣ CADASTRAR NOVO CLIENTE
+
+        public static bool Cadastrar(Clientes cliente)
+        {
+            using (var ctx = new LaFlorencaEntities())
+            {
+                ctx.Clientes.Add(cliente);  // adiciona ao contexto
+                return ctx.SaveChanges() > 0; // salva no banco
+            }
+        }
+
+
+        internal static void Inserir(Clientes novo){
+            using (var ctx = new LaFlorencaEntities())
+            {
+                ctx.Clientes.Add(novo);
+                ctx.SaveChanges();
+            }
+            
+        
+        }
+
+    }
+}
